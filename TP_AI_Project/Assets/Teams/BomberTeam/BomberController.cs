d82f178c4a18620d0cbd1f6daed35444a5250913
Leo <Aqu4men@gmail.com>
2022-11-03 18:41:14 +0100

    begin movements

--- TP_AI_Project/Assets/Teams/BomberTeam/BomberController.cs
+++ TP_AI_Project/Assets/Teams/BomberTeam/BomberController.cs
@@ -12,16 +12,34 @@ namespace BomberTeam {
 	public class BomberController : BaseSpaceShipController
 	{
 		public BehaviorTree BehaviorTree;
-		public override void Initialize(SpaceShipView spaceship, GameData data)
-		{
-			
-		}
+        public AnimationCurve AngleScoreCurve;
+        public AnimationCurve DistanceScore;
+        public List<WayPointView> Points;
+        public List<AsteroidView> Asteroids;
+        private Vector3 closest;
+        public float AngleView;
+        public override void Initialize(SpaceShipView spaceship, GameData data)
+        {
+            Points = data.WayPoints;
+            Asteroids = data.Asteroids;
+            closest = Points[0].Position;
+            foreach (WayPointView point in Points)
+            {
+                if (((Vector3)point.Position - transform.position).magnitude <= ((Vector3)closest - transform.position).magnitude)
+                {
+                    closest = point.Position;
 
-		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
+                }
+
+            }
+            Debug.Log(closest);
+
+        }
+
+        public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
 		{
 			SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
-			float thrust = 0f;
-			float targetOrient = spaceship.Orientation;
+			
 			
 			BehaviorTree.SetVariableValue("shipPosition", spaceship.Position);
 			BehaviorTree.SetVariableValue("shipOrientation", spaceship.Orientation);
@@ -38,8 +56,50 @@ namespace BomberTeam {
 			
 			bool useMineNext = (bool)BehaviorTree.GetVariable("useMineNext").GetValue();
 			if (useMineNext) BehaviorTree.SetVariableValue("useMineNext", false);
-			
-			return new InputData(thrust, targetOrient, shootNext, useMineNext, useShockwaveNext);
+
+
+
+            //Movement
+            float MaxScore = 0;
+            foreach (WayPointView point in Points)
+            {
+                Vector3 Tempdir = (Vector2)point.Position - spaceship.Position;
+                Tempdir = transform.InverseTransformDirection(Tempdir);
+                float Tempangle = Mathf.Abs(Mathf.Atan2(Tempdir.y, Tempdir.x) * Mathf.Rad2Deg);
+
+                float score = 1 / ((Vector3)point.Position - transform.position).magnitude;
+
+
+                if (Tempangle <= AngleView / 2)
+                {
+                    score *= (AngleScoreCurve.Evaluate(Tempangle / (AngleView / 2)));
+                   
+                }
+
+                if (((Vector3)point.Position - transform.position).magnitude <= (closest - transform.position).magnitude)
+                {
+                    score += DistanceScore.Evaluate(((Vector3)point.Position - transform.position).magnitude / 11);
+                    //Debug.Log(((Vector3)point.Position - transform.position).magnitude);
+                }
+
+                if (point.Owner == spaceship.Owner)
+                {
+                    score = 0;
+                }
+
+                if (score > MaxScore)
+                {
+                    MaxScore = score;
+                    closest = point.Position;
+                }
+
+
+            }
+            float thrust = 1f;
+            float angle = AimingHelpers.ComputeSteeringOrient(spaceship, closest, 1.2f);
+            float targetOrient = transform.rotation.z + angle;
+
+            return new InputData(thrust, targetOrient, shootNext, useMineNext, useShockwaveNext);
 		}
 	}
 
